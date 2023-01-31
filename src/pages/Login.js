0635bb60fcb06f2e8917a10087ec491a555fd442
kimiahasani <kn.improve@gmail.com>
2023-01-31 14:11:23 -0800

    create logo

---
+++ src/pages/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Login = () => {
+  return (
+    <>
+      <h1>SWEET HOME</h1>
+      <img src="" alt="sweet-home-logo"/>
+
+      <button>Sign in with Google</button>
+    </>
+  );
+};
+
+export default Login;
