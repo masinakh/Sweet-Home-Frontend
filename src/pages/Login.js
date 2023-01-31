df1dcf5e48a5a659936db5fb009064111dc502b4
masinakh <nakhjirimasi@gmail.com>
2023-01-31 15:21:40 -0800

    add login page and css

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,24 @@
 import React from "react";
-
+import "./Login.css";
 
 const Login = () => {
   return (
     <>
       <h1>SWEET HOME</h1>
-      <img src="" alt="sweet-home-logo"/>
-
-      <button>Sign in with Google</button>
+      <img src="./images/images.jpeg" alt="sweet-home-logo" />
+      <p>
+        This App motivate kids to do chores and establish good habits and
+        positive behaviors by taking responsibility and turn chores into a fun
+        game by achieving goals and scoring points,Then reward those who exceed
+        expectations by redeeming their points.
+      </p>
+      <div id="gSignInWrapper">
+        <span class="label">Sign in with:</span>
+        <div id="customBtn" class="customGPlusSignIn">
+          <span class="icon"></span>
+          <span class="buttonText">Google</span>
+        </div>
+      </div>
     </>
   );
 };
