9e47a5dd2b7ed8567f51bda1e086d9ae31089475
masinakh <nakhjirimasi@gmail.com>
2023-02-09 14:35:40 -0800

    add css

--- src/pages/MemberAccount.js
+++ src/pages/MemberAccount.js
@@ -5,7 +5,7 @@ import NewRewardForm from "../components/NewRewardForm";
 import ChoresToDo from "../components/ChoresToDo";
 import MemberSelectedRewards from "../components/MemberSelectedRewards";
 import Nav from "../components/Nav";
-import "./MemberAccount.css"
+import "./MemberAccount.css";
 
 const MemberAccount = ({
   rewardList,
@@ -15,35 +15,39 @@ const MemberAccount = ({
   addReward,
   member,
 }) => {
-
   return (
     <div className="memberAccountContainer">
-       <div className="welcome">
-      <h2>Welcome {member.name}</h2>
+      <div className="welcome">
+        <h2>Welcome {member.name}</h2>
       </div>
       <div className="nav">
-      <Nav member={member}></Nav>
+        <Nav member={member}></Nav>
       </div>
-      <div className="points"><h3>✨ YOUR POINTS: {member.points} ✨</h3></div>
-      
-      {/* <div className="rewardList"><RewardList
-        rewardList={rewardList}
-        deleteReward={deleteReward}
-        selectReward={selectReward}
-          />
-      </div> */}
-      {/* <div className="newRewardForm">{member.is_parent ? (
-        <NewRewardForm addReward={addReward} familyId={member.family_id} />
-      ) : (
-        <div></div>
-      )}
-      </div> */}
+      <div className="points">
+        <h3>✨ YOUR POINTS: {member.points} ✨</h3>
+      </div>
 
-      {/* <div className="memberSelectedReward"><MemberSelectedRewards
-        selectedReward={selectedReward}
-        name={member.name}
-      />
-      </div> */}
+      <div className="rewardList">
+        <RewardList
+          rewardList={rewardList}
+          deleteReward={deleteReward}
+          selectReward={selectReward}
+        />
+      </div>
+      <div className="newRewardForm">
+        {member.is_parent ? (
+          <NewRewardForm addReward={addReward} familyId={member.family_id} />
+        ) : (
+          <div></div>
+        )}
+      </div>
+
+      <div className="memberSelectedReward">
+        <MemberSelectedRewards
+          selectedReward={selectedReward}
+          name={member.name}
+        />
+      </div>
     </div>
   );
 };
